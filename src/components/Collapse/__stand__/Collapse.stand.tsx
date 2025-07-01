de5c3531cd142a698a83fd8b9c4c2b680b066055
gizeasy <gizeasy@gmail.com>
2025-07-01 16:55:38 +0300

    Update Collapse.stand.tsx

--- src/components/Collapse/__stand__/Collapse.stand.tsx
+++ src/components/Collapse/__stand__/Collapse.stand.tsx
@@ -4,7 +4,7 @@ export default createStand({
   title: 'Collapse',
   id: 'Collapse',
   group: 'components',
-  description: 'Компонент вывода данных в ячейке.',
+  description: 'Компонент для сворачивания таблицы',
   version: '0.6.0',
   status: 'stable',
   alias: ['шапка', 'сворачивание'],

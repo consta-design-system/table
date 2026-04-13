a2477bc1d2f4271c6407ad9d1f8698f205a85222
Mikhail Baev <baev2m@gmail.com>
2026-04-13 18:56:58 +0500

    chore(linters): fix all eslint/stylelint errors

--- src/utils/type-guards.ts
+++ src/utils/type-guards.ts
@@ -7,7 +7,7 @@ export const isNotNil = <T>(p: T): p is Exclude<T, undefined | null> =>
 export const isNumber = (value: unknown): value is number =>
   typeof value === 'number';
 
-export const isFunction = <T>(value: unknown): value is Function =>
+export const isFunction = (value: unknown): value is Function =>
   typeof value === 'function';
 
 export const isString = (value: unknown): value is string =>

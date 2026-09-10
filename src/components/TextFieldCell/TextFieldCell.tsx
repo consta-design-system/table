3be95f3254b8380de82c2d5ee92bc9d58604c83a
gizeasy <gizeasy@gmail.com>
2026-09-10 15:34:37 +0300

    Update TextFieldCell.tsx

--- src/components/TextFieldCell/TextFieldCell.tsx
+++ src/components/TextFieldCell/TextFieldCell.tsx
@@ -1,15 +1,25 @@
 import './TextFieldCell.css';
 
-import { useClickOutside } from '@consta/uikit/__internal__/src/hooks/useClickOutside';
+import { setRefs } from '@consta/uikit/__internal__/src/utils/setRef';
+import {
+  clickOutsideEffect,
+  factoryComponent,
+  keysEffect,
+} from '@consta/uikit/__internal__/src/utils/state';
 import {
   TextField,
   TextFieldProps,
   TextFieldPropValue,
 } from '@consta/uikit/TextFieldCanary';
-import { useFlag } from '@consta/uikit/useFlag';
-import { useForkRef } from '@consta/uikit/useForkRef';
-import { useKeysRef } from '@consta/uikit/useKeysRef';
-import React, { forwardRef, useEffect, useRef } from 'react';
+import {
+  action,
+  atom,
+  computed,
+  effect,
+  peek,
+  reatomBoolean,
+} from '@reatom/core';
+import React from 'react';
 
 import { DataCell } from '##/components/DataCell';
 import { cn } from '##/utils/bem';
@@ -44,166 +54,169 @@ export type TextFieldCellComponent = <TYPE extends string>(
   props: TextFieldCellProps<TYPE>,
 ) => React.ReactNode | null;
 
-const TextFieldCellRender = (
-  props: SplitProps,
-  ref: React.Ref<HTMLDivElement>,
-) => {
-  const {
-    className,
-    size = 'm',
-    lineClamp,
-    type,
-    value,
-    readModeRender = readModeRenderDefault,
-    level,
-    truncate,
-    defaultValue,
-    onChange,
-    id,
-    name,
-    disabled,
-    maxLength,
-    onFocus,
-    onBlur,
-    placeholder,
-    leftSide,
-    rightSide,
-    clearButton,
-    iconClear,
-    autoComplete,
-    readOnly,
-    tabIndex,
-    ariaLabel,
-    iconSize,
-    onClear,
-    inputRef: inputRefProp,
-    onKeyUp,
-    onKeyUpCapture,
-    onKeyDown,
-    onKeyDownCapture,
-    onCopy,
-    onCopyCapture,
-    onCut,
-    onCutCapture,
-    onPaste,
-    onPasteCapture,
-    onWheel,
-    max,
-    min,
-    step,
-    incrementButtons,
-    iconShowPassword,
-    iconHidePassword,
-    resize,
-    minRows,
-    maxRows,
-    rows,
-    renderValueItem,
-    inputValue,
-    onInputChange,
-    status,
-    indicator,
-    readonly,
-    ...restProps
-  } = props;
-  const refRoot = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLInputElement | HTMLTextAreaElement>(null);
-  const inputRefForked = useForkRef([inputRef, inputRefProp]);
+export const TextFieldCell = factoryComponent(
+  (initProps: SplitProps, propsAtom) => {
+    const rootElAtom = atom<HTMLDivElement | null>(null);
+    const inputElAtom = atom<HTMLInputElement | null>(null);
 
-  const [editMode, setEditMode] = useFlag();
+    const inputRef = action((el: HTMLInputElement | null) =>
+      setRefs([inputElAtom.set, propsAtom().inputRef], el),
+    );
 
-  useClickOutside({
-    isActive: editMode,
-    ignoreClicksInsideRefs: [refRoot],
-    handler: setEditMode.off,
-  });
+    const rootRef = action((el: HTMLDivElement | null) =>
+      setRefs([rootElAtom.set, propsAtom().ref], el),
+    );
 
-  useKeysRef({
-    isActive: editMode,
-    ref: refRoot,
-    keys: {
-      Escape: setEditMode.off,
-    },
-  });
+    const editModeAtom = reatomBoolean(false);
 
-  useEffect(() => {
-    if (editMode) {
-      inputRef.current?.focus();
-    }
-  }, [editMode]);
+    clickOutsideEffect({
+      isActiveAtom: editModeAtom,
+      ignoreClicksElementsAtom: computed(() => [rootElAtom()]),
+      handler: editModeAtom.setFalse,
+    });
 
-  const textFiledProps = {
-    className: cnTextFieldCell('Field'),
-    view: 'clear',
-    onDoubleClick: setEditMode.on,
-    value,
-    type,
-    inputRef: inputRefForked,
-    defaultValue,
-    onChange,
-    id,
-    name,
-    disabled,
-    maxLength,
-    onFocus,
-    onBlur,
-    placeholder,
-    leftSide,
-    rightSide,
-    clearButton,
-    iconClear,
-    autoComplete,
-    readOnly,
-    tabIndex,
-    ariaLabel,
-    iconSize,
-    onClear,
-    onKeyUp,
-    onKeyUpCapture,
-    onKeyDown,
-    onKeyDownCapture,
-    max,
-    min,
-    step,
-    incrementButtons,
-    iconShowPassword,
-    iconHidePassword,
-    resize,
-    minRows,
-    maxRows,
-    rows,
-    renderValueItem,
-    inputValue,
-    onInputChange,
-    onCopy,
-    onCopyCapture,
-    onCut,
-    onCutCapture,
-    onPaste,
-    onPasteCapture,
-    onWheel,
-    size,
-  } as const;
+    keysEffect({
+      isActiveAtom: editModeAtom,
+      elAtom: rootElAtom,
+      keysAtom: computed(() => ({
+        Escape: editModeAtom.setFalse,
+      })),
+    });
 
-  return (
-    <DataCell
-      {...restProps}
-      className={cnTextFieldCell({ size }, [className])}
-      ref={useForkRef([refRoot, ref])}
-      onDoubleClick={readonly ? undefined : setEditMode.on}
-      size={size}
-      lineClamp={lineClamp}
-      indicator={!editMode ? indicator : undefined}
-      level={level}
-      truncate={truncate}
-      data-cell-edit-mode={editMode}
-      data-cell-status={editMode ? status : undefined}
-    >
-      {editMode ? <TextField {...textFiledProps} /> : readModeRender(value)}
-    </DataCell>
-  );
-};
+    effect(() => editModeAtom() && inputElAtom()?.focus());
 
-export const TextFieldCell = forwardRef(
-  TextFieldCellRender,
+    return (props) => {
+      const {
+        className,
+        size = 'm',
+        lineClamp,
+        type,
+        value,
+        readModeRender = readModeRenderDefault,
+        level,
+        truncate,
+        defaultValue,
+        onChange,
+        id,
+        name,
+        disabled,
+        maxLength,
+        onFocus,
+        onBlur,
+        placeholder,
+        leftSide,
+        rightSide,
+        clearButton,
+        iconClear,
+        autoComplete,
+        readOnly,
+        tabIndex,
+        ariaLabel,
+        iconSize,
+        onClear,
+        inputRef: inputRefProp,
+        onKeyUp,
+        onKeyUpCapture,
+        onKeyDown,
+        onKeyDownCapture,
+        onCopy,
+        onCopyCapture,
+        onCut,
+        onCutCapture,
+        onPaste,
+        onPasteCapture,
+        onWheel,
+        max,
+        min,
+        step,
+        incrementButtons,
+        iconShowPassword,
+        iconHidePassword,
+        resize,
+        minRows,
+        maxRows,
+        rows,
+        renderValueItem,
+        inputValue,
+        onInputChange,
+        status,
+        indicator,
+        readonly,
+        ...restProps
+      } = props;
+
+      const textFiledProps = {
+        className: cnTextFieldCell('Field'),
+        view: 'clear',
+        onDoubleClick: editModeAtom.setTrue,
+        value,
+        type,
+        inputRef,
+        defaultValue,
+        onChange,
+        id,
+        name,
+        disabled,
+        maxLength,
+        onFocus,
+        onBlur,
+        placeholder,
+        leftSide,
+        rightSide,
+        clearButton,
+        iconClear,
+        autoComplete,
+        readOnly,
+        tabIndex,
+        ariaLabel,
+        iconSize,
+        onClear,
+        onKeyUp,
+        onKeyUpCapture,
+        onKeyDown,
+        onKeyDownCapture,
+        max,
+        min,
+        step,
+        incrementButtons,
+        iconShowPassword,
+        iconHidePassword,
+        resize,
+        minRows,
+        maxRows,
+        rows,
+        renderValueItem,
+        inputValue,
+        onInputChange,
+        onCopy,
+        onCopyCapture,
+        onCut,
+        onCutCapture,
+        onPaste,
+        onPasteCapture,
+        onWheel,
+        size,
+      } as const;
+
+      const editMode = editModeAtom();
+
+      return (
+        <DataCell
+          {...restProps}
+          className={cnTextFieldCell({ size }, [className])}
+          ref={rootRef}
+          onDoubleClick={readonly ? undefined : editModeAtom.setTrue}
+          size={size}
+          lineClamp={lineClamp}
+          indicator={!editMode ? indicator : undefined}
+          level={level}
+          truncate={truncate}
+          data-cell-edit-mode={editMode}
+          data-cell-status={editMode ? status : undefined}
+        >
+          {editMode ? <TextField {...textFiledProps} /> : readModeRender(value)}
+        </DataCell>
+      );
+    };
+  },
 ) as TextFieldCellComponent;
